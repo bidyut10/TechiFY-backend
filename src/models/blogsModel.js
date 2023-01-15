8e94c9b801004c29ee30b96e28b7c5a036cd62dc
Bidyut Kundu <106534797+bidyut10@users.noreply.github.com>
2023-01-15 15:21:44 +0530

    Update blogsModel.js
    update

--- src/models/blogsModel.js
+++ src/models/blogsModel.js
@@ -6,9 +6,9 @@ const blogsSchema = new mongoose.Schema(
   {
     authorId: { type: ObjectId, required: true, ref: "Author", trim: true },
     title: { type: String, required: true, trim: true },
-    subtitle: { type: String, required: true, trim: true },
+    location: { type: String, required: true, trim: true },
     description: { type: String, required: true, trim: true },
-    summary: { type: String, required: true, trim: true },
+    imgUrl: { type: String, required: true, trim: true },
     deletedAt: { type: Boolean, default: false, trim: true },
   },
   { timestamps: true }
